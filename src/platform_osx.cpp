168dcec2870597cb3386f189765296be282ed1df
Matthew Madden <matthewkayin@gmail.com>
2024-07-01 15:44:05 -0500

    Add OSX clock
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- src/platform_osx.cpp
+++ src/platform_osx.cpp
@@ -2,6 +2,18 @@
 
 #ifdef PLATFORM_OSX
 
+#include <cstdio>
+#include <mach/mach_time.h>
+
+double platform_get_absolute_time() {
+    mach_timebase_info_data_t clock_timebase;
+    mach_timebase_info(&clock_timebase);
+
+    uint64_t mach_absolute = mach_absolute_time();
+    uint64_t nanos = (double)(mach_absolute * (uint64_t)clock_timebase.numer) / (double)clock_timebase.denom;
+    return nanos / 1.0e9;
+}
+
 void platform_console_write(const char* message) {
     printf("\x1B[0m%s", message);
 }
