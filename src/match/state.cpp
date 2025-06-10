56f5af2054225b1c5b8c1affa65d6850800c289d
Matthew Madden <matthewkayin@gmail.com>
2025-06-10 05:51:55 -0500

    fix desync bug with cannon fire position

--- src/match/state.cpp
+++ src/match/state.cpp
@@ -784,20 +784,31 @@ EntityId match_create_entity(MatchState& state, EntityType type, ivec2 cell, uin
 EntityId match_create_goldmine(MatchState& state, ivec2 cell, uint32_t gold_left) {
     Entity entity;
     entity.type = ENTITY_GOLDMINE;
+    entity.mode = MODE_GOLDMINE;
     entity.player_id = PLAYER_NONE;
     entity.flags = 0;
-    entity.mode = MODE_GOLDMINE;
 
     entity.cell = cell;
     entity.position = fvec2(entity.cell * TILE_SIZE);
     entity.direction = DIRECTION_SOUTH;
 
+    entity.health = 0;
+    entity.energy = 0;
+    entity.target = (Target) { .type = TARGET_NONE };
+    entity.rally_point = ivec2(-1, -1);
+    entity.pathfind_attempts = 0;
+    entity.timer = 0;
+    entity.animation = animation_create(ANIMATION_UNIT_IDLE);
     entity.garrison_id = ID_NULL;
+    entity.cooldown_timer = 0;
     entity.gold_held = gold_left;
+    entity.gold_mine_id = ID_NULL;
 
     entity.taking_damage_counter = 0;
     entity.taking_damage_timer = 0;
     entity.health_regen_timer = 0;
+    entity.fire_damage_timer = 0;
+    entity.energy_regen_timer = 0;
 
     EntityId id = state.entities.push_back(entity);
     map_set_cell_rect(state.map, CELL_LAYER_GROUND, entity.cell, entity_get_data(entity.type).cell_size, (Cell) {
@@ -2426,9 +2437,8 @@ void match_entity_attack_target(MatchState& state, EntityId attacker_id) {
 
         hit_position = ivec2(defender_rect.x - TILE_SIZE + hit_x, defender_rect.y - TILE_SIZE + hit_y);
     } else {
-        hit_position = ivec2(
-            defender_rect.x + (defender_rect.w / 4) + (lcg_rand(&state.lcg_seed) % (defender_rect.w / 2)),
-            defender_rect.y + (defender_rect.h / 4) + (lcg_rand(&state.lcg_seed) % (defender_rect.h / 2)));
+        hit_position.x = defender_rect.x + (defender_rect.w / 4) + (lcg_rand(&state.lcg_seed) % (defender_rect.w / 2));
+        hit_position.y = defender_rect.y + (defender_rect.h / 4) + (lcg_rand(&state.lcg_seed) % (defender_rect.h / 2));
     }
     log_trace("DESYNC attack hit_position %vi", &hit_position);
 
