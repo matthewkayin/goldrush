56aa37eb347706393e3aa24b7ccd0fb2ba77c280
Matthew Madden <matthewkayin@gmail.com>
2025-12-24 05:16:16 -0600

    tweaked player ui colors to be less easter

--- src/render/ui_color.h
+++ src/render/ui_color.h
@@ -3,8 +3,8 @@
 #include <SDL3/SDL.h>
 
 const SDL_Color PLAYER_UI_COLOR[4] = {
-    (SDL_Color) { .r = 96, .g = 151, .b = 191, .a = 255 }, // Blue
+    (SDL_Color) { .r = 106, .g = 151, .b = 173, .a = 255 }, // Blue
     (SDL_Color) { .r = 204, .g = 108, .b = 106, .a = 255 }, // Red
-    (SDL_Color) { .r = 108, .g = 166, .b = 121, .a = 255 }, // Green
-    (SDL_Color) { .r = 156, .g = 133, .b = 204, .a = 255 } // Purple
+    (SDL_Color) { .r = 89, .g = 153, .b = 121, .a = 255 }, // Green
+    (SDL_Color) { .r = 163, .g = 135, .b = 173, .a = 255 } // Purple
 };
