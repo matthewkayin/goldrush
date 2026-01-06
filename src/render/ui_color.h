61f7b019de63492b69a0ce29ad04ece303a3da53
Matthew Madden <matthewkayin@gmail.com>
2026-01-06 04:30:28 -0600

    bumped minimap colors saturation

--- src/render/ui_color.h
+++ src/render/ui_color.h
@@ -4,7 +4,7 @@
 
 const SDL_Color PLAYER_UI_COLOR[4] = {
     (SDL_Color) { .r = 96, .g = 146, .b = 166, .a = 255 }, // Blue
-    (SDL_Color) { .r = 204, .g = 108, .b = 106, .a = 255 }, // Red
-    (SDL_Color) { .r = 112, .g = 158, .b = 103, .a = 255 }, // Green
-    (SDL_Color) { .r = 163, .g = 135, .b = 173, .a = 255 } // Purple
+    (SDL_Color) { .r = 204, .g = 94, .b = 92, .a = 255 }, // Red
+    (SDL_Color) { .r = 99, .g = 156, .b = 87, .a = 255 }, // Green
+    (SDL_Color) { .r = 154, .g = 106, .b = 171, .a = 255 } // Purple
 };
